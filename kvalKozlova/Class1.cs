0ef06275b971c45fc647a0180d6db8475e71de7b
xeanko@yandex.ru <xeanko@yandex.ru>
2023-07-05 09:27:28 +0300

    Создание класса Team

--- kvalKozlova/Class1.cs
+++ kvalKozlova/Class1.cs
@@ -8,6 +8,28 @@ namespace kvalKozlova
 {
     public class Class1
     {
-
+        // Козлова К.А. 31ИС
+        // класс Team
+        public class Team
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public List<Player> Players = new List<Player>();
+            public Team(string nm, string ad)
+            {
+                Name = nm;
+                Address = ad;
+            }
+            public void ShowAll()
+            {
+                Console.WriteLine($"Команда: Название - {Name}, Адрес - {Address}");
+                Console.WriteLine("Состав команды:");
+                Players.Sort();
+                foreach (Player players in Player)
+                {
+                    players.Show();
+                }
+            }
+        }
     }
 }
